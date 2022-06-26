8d7f028493cca53738d567efd939970c9c895f06
Nuunamnir <mail@nuunamnir.com>
2022-06-26 09:23:09 +0000

    feat: add utm parameter handling

--- src/backend/server.go
+++ src/backend/server.go
@@ -6,6 +6,7 @@ import (
 	"time"
 	"log"
 	"net/http"
+	"net/url"
 	"github.com/rs/cors"
 	"encoding/json"
 	"github.com/influxdata/influxdb-client-go/v2/api"
@@ -32,44 +33,61 @@ type Payload struct {
 func (ich *InfluxClientHandler) Monitor(w http.ResponseWriter, r *http.Request) {
 	var p Payload
 	err := json.NewDecoder(r.Body).Decode(&p)
-	if err != nil {
-		log.Fatal("Cannot read payload.")
+	if err == nil {
+		CurrentPage, error := url.Parse(p.Href)
+		var QSource = ""
+		var QMedium = ""
+		var QCampaign = ""
+		var QTerm = ""
+		var QContent = ""
+		if error == nil {			
+			QSource = CurrentPage.Query().Get("utm_source")
+			QMedium = CurrentPage.Query().Get("utm_medium")
+			QCampaign = CurrentPage.Query().Get("utm_campaign")
+			QTerm = CurrentPage.Query().Get("utm_term")
+			QContent = CurrentPage.Query().Get("utm_content")
+		} 
+
+		tags := map[string]string{
+			"website_id": p.NId,	
+			"visitor_ip": r.RemoteAddr,
+			"event_type": p.EventType,
+		}
+		fields := map[string]interface{}{
+			"click_target": p.ClickTarget,
+			"closest_link_href": p.ClosestLinkHref,
+			"user_agent": p.UserAgent,
+			"current_page": p.Href,
+			"referrer": p.Referrer,
+			"cpu_cores": p.HardwareConcurrency,
+			"device_memory": p.DeviceMemory,
+			"max_touch_points": p.MaxTouchPoints,
+			"language": p.Language,
+			"cookie_support": p.CookieEnabled,
+			"pdf_support": p.PDFViewerEnabled,
+			"online": p.OnLine,
+			"q_source": QSource,
+			"q_medium": QMedium,
+			"q_campaign": QCampaign,
+			"q_term": QTerm,
+			"q_content": QContent,
+		}
+	
+		influx_p := influxdb2.NewPoint("event",
+			tags,
+			fields,
+			time.Now())
+		ich.WriteAPI.WritePoint(influx_p)
+		ich.WriteAPI.Flush() // should probably be removed after testing, might have performance impact
 	}
 	w.WriteHeader(http.StatusOK)
-	
-	tags := map[string]string{
-		"website_id": p.NId,	
-		"visitor_ip": r.RemoteAddr,
-		"event_type": p.EventType,
-	}
-	fields := map[string]interface{}{
-		"click_target": p.ClickTarget,
-		"closest_link_href": p.ClosestLinkHref,
-		"user_agent": p.UserAgent,
-		"current_page": p.Href,
-		"referrer": p.Referrer,
-		"cpu_cores": p.HardwareConcurrency,
-		"device_memory": p.DeviceMemory,
-		"max_touch_points": p.MaxTouchPoints,
-		"language": p.Language,
-		"cookie_support": p.CookieEnabled,
-		"pdf_support": p.PDFViewerEnabled,
-		"online": p.OnLine,
-	}
-
-	influx_p := influxdb2.NewPoint("event",
-        tags,
-        fields,
-        time.Now())
-    ich.WriteAPI.WritePoint(influx_p)
-	ich.WriteAPI.Flush() // should probably be removed after testing, might have performance impact
 }
 
 func (ich *InfluxClientHandler) MonitorPixel(w http.ResponseWriter, r *http.Request) {
-	keys, ok := r.URL.Query()["nid"]
-	if ok {
+	QNid := r.URL.Query().Get("nid")
+	if QNid != "" {
 		tags := map[string]string{
-			"website_id": keys[0],	
+			"website_id": QNid,	
 			"visitor_ip": r.RemoteAddr,
 			"event_type": "page_pixel",
 		}
@@ -102,7 +120,7 @@ type InfluxClientHandler struct {
 
 func main() {
 	log.Println("nuunamnir website analytics tracking server started ...")
-	godotenv.Load("../data/input/credentials.env")
+	godotenv.Load("../../data/input/credentials.env")
 	dbToken := os.Getenv("INFLUXDB_TOKEN")
 	dbURL := os.Getenv("INFLUXDB_URL")
 	dbOrganization := os.Getenv("INFLUXDB_ORGANIZATION")
